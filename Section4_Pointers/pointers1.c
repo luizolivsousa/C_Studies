b6ddadfd3ca8c73d8d954839bcf422224cd8d256
luizolivsousa <luizoliveduardo@gmail.com>
2023-07-01 17:09:32 -0400

    Understanding pointers better

--- Section4_Pointers/pointers1.c
+++ Section4_Pointers/pointers1.c
@@ -6,17 +6,18 @@ int main() {
     // When a variable is declared, the language selects a space in memory to alocate the value.
     int value = 10;
 
-    //%p is used to print memory and it requires the (void *). Its means the value is cast to a generic pointer
-    //  
-    printf("Value's value is %d. It's memory address is %d\n", value, &value);
+    //%p is used to print memory and it requires the (void *). Its means the value is cast to a generic pointer 
+    printf("Value's value is %d. It's memory address is %p\n", value, (void *)&value);
+    // Since the agument for increment function is an int*, it receives and memory address.
     increment(&value);
-    printf("Value's value after incrementing is %d. It's memory address is %d\n",
-            value, &value);
+    printf("Value's value after incrementing is %d. It's memory address is %p\n",
+            value, (void *)&value);
 }
 
-// Will receive a memory address that points to an integer.
+// Will receive a pointer to an integer.
 void increment(int* value) {
     // Now everything has the same memory address.
-    printf("Functions parameter is %d and it's memory address is %d\n", (*value), value);
+    printf("Functions parameter is %d and it's memory address is %p\n", (*value), (void *)value);
+    // Dereferencing the pointer, which means accessing it's numeric value.
     ++(*value);
 }

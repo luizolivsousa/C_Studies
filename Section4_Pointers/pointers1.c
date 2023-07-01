297e5174474ca0ebef8b032ba7ab1f76d1991691
luizolivsousa <luizoliveduardo@gmail.com>
2023-07-01 17:02:32 -0400

    Same memory adress for all variables

--- Section4_Pointers/pointers1.c
+++ Section4_Pointers/pointers1.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-void increment(int value);
+void increment(int* value);
 
 int main() {
     // When a variable is declared, the language selects a space in memory to alocate the value.
     int value = 10;
 
-    printf("Value value is %d. It's memory address is %d\n", value, &value);
-    increment(value);
-    printf("Value value after incrementing is %d. It's memory address is %d\n",
+    //%p is used to print memory and it requires the (void *). Its means the value is cast to a generic pointer
+    //  
+    printf("Value's value is %d. It's memory address is %d\n", value, &value);
+    increment(&value);
+    printf("Value's value after incrementing is %d. It's memory address is %d\n",
             value, &value);
 }
 
-void increment(int value) {
-    // This valuable has a different memory address than the value in the main funcion.
-    printf("Functions parameter is %d and it's memory address is %d\n", value, &value);
-    ++value;
+// Will receive a memory address that points to an integer.
+void increment(int* value) {
+    // Now everything has the same memory address.
+    printf("Functions parameter is %d and it's memory address is %d\n", (*value), value);
+    ++(*value);
 }
